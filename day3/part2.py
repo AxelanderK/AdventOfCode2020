6b756642b82ae8936137c2f64582484d7d83a9ae
Alexander Karlsson <info@alexanderk.se>
2020-12-09 20:06:21 +0100

    Day3 finished

--- day3/part2.py
+++ day3/part2.py
@@ -1,10 +1,11 @@
+from functools import reduce
+
 entries = list(open('day3/input.txt','r').read().split('\n'))
 
 def calThrees(items, rows, cols):
     count = 0
     row = 0
     col = 0
-    
     while row < len(items)-1:
         row += rows
         col += cols
@@ -17,4 +18,17 @@ items = []
 for e in entries:
     items.append(e * 100)
 
-print(calThrees(items, 1, 3))
+slope = []
+
+slope.append(calThrees(items, 1, 1))
+slope.append(calThrees(items, 1, 3))
+slope.append(calThrees(items, 1, 5))
+slope.append(calThrees(items, 1, 7))
+slope.append(calThrees(items, 2, 1))
+
+result = 0
+for x in slope:
+    print(x)
+    result += result * x
+
+print(reduce(lambda x, y: x*y, slope))
